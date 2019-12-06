3ab8d0dbddcf2478cf1b0961b4a431faaae98caa
BrAcInhO <bracinho2@hotmail.com>
2019-12-05 22:26:41 -0300

    Atualizacoes diversas.

---
+++ src/main/java/voy/me/pay/entity/Generic.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package voy.me.pay.entity;
+
+/**
+ *
+ * @author bracinho2
+ */
+public abstract class Generic {
+    
+    public abstract Long getId();
+    
+}
